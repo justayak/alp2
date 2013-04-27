5ee2b67830ce00fe2863f00319adb3281cce925c
justayak <justayak@googlemail.com>
2013-04-27 15:30:51 +0200

    aufgabe2 1.

--- julian/aufgabe2.py
+++ julian/aufgabe2.py
@@ -1,2 +1,19 @@
 #Aufgabe 1: n=44 Erg=[20,20,2,2]
 
+def change (n): 
+	coins = [200,100,50,20,10,5,2,1]
+	result = []
+	for coin in coins:
+		count = n // coin
+		result += [coin] * count
+		n = n % coin		
+	return result
+		
+		
+		
+money = change(338)
+
+for coin in money:
+	print(coin)
+	
+	
