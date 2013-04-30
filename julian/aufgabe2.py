f8ab77eaaa2c629187a0b1ded5b45e0802ebfa85
justayak <justayak@googlemail.com>
2013-04-30 17:44:53 +0200

    aufgabe 2

--- julian/aufgabe2.py
+++ julian/aufgabe2.py
@@ -1,4 +1,5 @@
 #Aufgabe 1: n=44 Erg=[20,20,2,2]
+from collections import deque
 
 def change (n): 
 	coins = [200,100,50,20,10,5,2,1]
@@ -9,10 +10,20 @@ def change (n):
 		n = n % coin		
 	return result
 		
-		
-		
-money = change(338)
+def changeWithWhile (n): 
+	coins = deque([200,100,50,20,10,5,2,1])
+	result = []
+	while n != 0:
+		coin = coins.popleft()
+		count = n // coin
+		result += [coin] * count
+		n = n % coin		
+	return result
+				
+inp = int(input("Geld: "));		
+money = changeWithWhile(inp)
 
+# Rückgabe wie in Tutorium besprochen
 for coin in money:
 	print(coin)
 	
