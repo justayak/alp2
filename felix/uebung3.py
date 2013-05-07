bc7e7f0ba6f181088d34683ba1c2f9e553a98155
festeinbach <felix.steinbach@fu-berlin.de>
2013-05-07 15:39:57 +0200

    Aufgabe 2

--- felix/uebung3.py
+++ felix/uebung3.py
@@ -10,3 +10,31 @@ def mersenne(n):
 a = int(input("Zahl eingeben:"))
 print(mersenne(a))
 
+
+def majority (array):
+	if array == []:
+		return None
+	count1 = 0
+	current = array[0]
+	i = 0
+	while i<len(array):
+		if array[i]  == current:
+			count1 = count1 + 1
+		else:
+			count1 = count1 - 1
+		if count1 == 0:
+			current = array[i]
+			count1 = 1
+		i+=1
+	count2 = 0
+	i=0
+	while i<len(array):
+		if array[i] == current:
+			count2 = count2 + 1
+		i+=1
+	if count2 > len(array)/2:
+		return current
+	return None
+
+array = [1,1,1,2,2,3,4,1,1]
+print(majority(array))
