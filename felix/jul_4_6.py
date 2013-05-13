146f8dd60c04b09976abd50c8f04f59b793e5302
justayak <justayak@googlemail.com>
2013-05-13 23:00:01 +0200

    update nun funzts

--- felix/jul_4_6.py
+++ felix/jul_4_6.py
@@ -1,68 +1,77 @@
 import math
 
+def parent(i):
+	return int(math.ceil((i -1)/2))	
 def left(i):
 	return 2*i+1
 def right(i):
 	return (2*i)+2
 	
-def parent(i):
-	return int(math.ceil(i/2)) -1
-	
 #Die processList wird in eine Heap-Struktur transformiert 
 def buildPriorityQueue( processList ):
-	return sorted(processList)
+	return sorted(processList, key=lambda process:process[0])
  
 # child = index, parent = index
 # stoppt, wenn 0 erreicht wird oder parent <= child
 def _bubbleUp(list,child, prnt):
 	if child == 0 or list[child] >= list[prnt]:
+		m = ">> " + repr(list[child]) + " >= " + repr(list[prnt]) + " ]or : " + repr(child == 0)
+		print (m)
 		return
 	else :
 		list[child], list[prnt] = list[prnt], list[child]
 		newParent = parent(prnt)
 		_bubbleUp(list, prnt, newParent)
-	
-def _printHeap(heap):
-	return 
-	
-def _height(heap, current = -1):
-	if current == -1
-		current = len(heap)-1
-	
-	
+		
 def insert( priorityQueue, newProcess ): 
 	nextSlot = len(priorityQueue)
 	priorityQueue.append(newProcess)
 	prnt = parent(nextSlot)
 	_bubbleUp(priorityQueue,nextSlot,prnt)
 	
-	
- #Ein neuer Prozess wird in dem priorityQueue eingefuegt 
 
 def isEmpty( priorityQueue ) :
-	return None
-
- #gibt einen Wahrheitswert als Rueckgabewert, je nachdem, ob die 
-
- #Prioritaetswarteschlange leer ist oder nicht """
+	return not priorityQueue
+	
 def removeProcess( priorityQueue ):
-	return None
- #Der Prozess mit der hoechsten Prioritaet wird aus der priorityQueue
+	if isEmpty(priorityQueue):
+		return None
+	result,priorityQueue[0] = priorityQueue[0], priorityQueue.pop()
+	priorityQueue.sort(key=lambda process:process[0])
+	return result
 
- #entfernt und als Ergebnis der Funktion zurueckgegeben """
-list=[(4,"la"),(3,"hd"),(10,"jhg"),(7,"sde"),(8,"fsdfs"),(0,"gfsgs")]
+list=[(4,"a"),(3,"b"),(10,"c"),(7,"d"),(8,"e"),(0,"f")]
 heap = buildPriorityQueue(list)
+
+v = (-1,"h")
+insert(heap, v)
+
+v = (1,"g")
+insert(heap, v)
+
+v = (15,"i")
+insert(heap, v)
+
+v = (12,"j")
+insert(heap, v)
+
+v = (16,"k")
+insert(heap, v)
+
+v = (-3,"k")
+insert(heap, v)
+
+v = (-2,"k")
+insert(heap, v)
 
 print (heap)
 
-v = (1,"Felix!")
-insert(heap, v)
+v = removeProcess(heap)
 
-v = (-12,"Felix!")
-insert(heap, v)
-
-v = (99,"Felix!")
-insert(heap, v)
+v = removeProcess(heap)
+v = removeProcess(heap)
+v = removeProcess(heap)
+v = removeProcess(heap)
 
 print (heap)
 
