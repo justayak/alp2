0be315af45ce14d684efb99e31221d380b3eaaa5
justayak <jutanke@googlemail.com>
2013-05-13 13:17:25 +0200

    5 funzt

--- julian/aufgabe4_6.py
+++ julian/aufgabe4_6.py
@@ -1,23 +1,39 @@
-#Aufgabe 5
-def approxMedianPos(list, median):
-	for i,number in enumerate(list):
-		if number == median:
-			return i
-		if abs(number) > abs(median):
-			return i-1
-	raise Exception("lol hier haett ich nie hingedurft!")		
+#Aufgabe 5	
 
+def miniSubsetSumAlt(list):
+	list = sorted(list)  #Timsort:  				O(n*log(n))
+	median = sum(list) // len(list)  #				O(n)
+	#
+	print (median)
+	i =  0 if median >= 0 else len(list)-1
+	acc = 1 if median >= 0 else -1
+	end =  len(list)-1 if median >= 0 else len(list)-1
+	result = []		
+	while i < end if median >= 0 else i > end:
+		print (i)
+		j = i + acc
+		while j < end if median >= 0 else j > end:			
+			current = list[i] + list[j]		
+			if current == median:
+				result.append((list[i], list[j]))
+				break			
+			if current >  median:
+				break
+			j = j + acc
+		i = i + acc
+	return result
+	
 def miniSubsetSum(list):
 	list = sorted(list)  #Timsort:  				O(n*log(n))
 	median = sum(list) // len(list)  #				O(n)
 	#
 	print (median)
-	approxMedianPosition = approxMedianPos(list, median)  #			O(n-1)
-	i =  0 if median >= 0 else approxMedianPosition
-	end =  approxMedianPosition if median >= 0 else len(list)-1
+	i =  0 
+	acc = 1 
+	end =  len(list)-1 
 	result = []		
 	while i < end:
-		j = i + 1
+		j = i + acc
 		while j < end:			
 			current = list[i] + list[j]		
 			if current == median:
@@ -25,15 +41,15 @@ def miniSubsetSum(list):
 				break			
 			if current >  median:
 				break
-			j = j + 1
-		i = i + 1
+			j = j + acc
+		i = i + acc
 	return result
 
 
 def run3():
-	v = [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12]
+	#v = [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12]
 	v = [4,3,-1,-34,12, 4,5,6,3,11,12,13,1,2,3,4,5,6,7,-6,-1,-5,-12,0]
-	v = [-1,2,-3,4,-5,6,-7,8,-9,10,12]
+	#v = [-1,2,-3,4,-5,6,-7,8,-9,10,12]
 	#v = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 	result = miniSubsetSum(v)
 	print (result)
