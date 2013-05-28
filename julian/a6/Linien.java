bfc6e0c45198e01dc0dcfffc2c2f961e544ed1f2
justayak <justayak@googlemail.com>
2013-05-28 22:52:44 +0200

    blabla shit

---
+++ julian/a6/Linien.java
@@ -0,0 +1,23 @@
+package a6;
+
+public class Linien {
+    public static void main(String[] args) {
+        double t = Math.sqrt(3.0) / 2.0;
+        for (int i = 0; i < 50; i = i + 1) {
+            float z = (float) i;
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.line(0.0, z/50, 1.0-(z/50), 0.0);
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.line(1.0, z/50, 1.0-(z/50), 1);
+            StdDraw.setPenColor(StdDraw.MAGENTA);
+            StdDraw.line(z/50, 1-(z/50), 1.0, z/50);
+            StdDraw.setPenColor(StdDraw.GREEN);
+            StdDraw.line(z/50, z/50, 1.0, 1-(z/50));
+        }
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.line(0.0, 0.0, 0.0, 1.0);
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.line(0.0, 1.0, 1.0, 1.0);
+        
+    }
+}
