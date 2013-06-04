3559dd356d2cf81d49a91c08acd2cc9cec09b2c8
justayak <justayak@googlemail.com>
2013-06-04 19:45:51 +0200

    jul

---
+++ julian/a7/aufgabe1/Main.java
@@ -0,0 +1,11 @@
+package a7.aufgabe1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Baka
+ * Date: 04.06.13
+ * Time: 18:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
