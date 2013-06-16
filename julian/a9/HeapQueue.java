0783a660720dc92098862b76f75d88c054aefe9d
justayak <justayak@googlemail.com>
2013-06-16 21:58:11 +0200

    jul stuff aufg9 2

--- julian/a9/HeapQueue.java
+++ julian/a9/HeapQueue.java
@@ -12,14 +12,18 @@ public final class HeapQueue<P extends Comparable<P>,D> {
     private final int CAPACITY = 10;
     private int capacity;
     private Data<P,D>[] array;
+    private int current = -1;
 
     public HeapQueue(){
-        this.array = ( Data<P,D>[]) new Object[CAPACITY];
+        this.array = new Data[CAPACITY];
         this.capacity = CAPACITY;
     }
 
     public void store(P prority, D data){
-
+        Data<P,D> e = new Data<P, D>(prority,data);
+        this.current += 1;
+        this.array[this.current] = e;
+        this.heapify(this.current);
     }
 
     public D remove() throws Queue.EmptyQueueException{
@@ -33,6 +37,32 @@ public final class HeapQueue<P extends Comparable<P>,D> {
     // ========================
     // P R I V A T E
     // ========================
+
+    private void heapify(){
+
+    }
+
+    private void heapify(int p){
+        int left = this.left(p);
+        int right = this.right(p);
+        int biggest = -1;
+        int heapSize = current;
+        if (left <= heapSize && (this.array[left].greater(this.array[p]))){
+            biggest = left;
+        }else {
+            biggest = p;
+        }
+
+        if (right <= heapSize && (this.array[right].greater(this.array[biggest]))){
+            biggest = right;
+        }
+
+        if (biggest != p){
+            Data<P,D> temp = this.array[biggest];
+            this.array[biggest] = this.array[p];
+            this.array[p] = temp;
+        }
+    }
 
     private void resize(){
         Data[] newArray = (Data[]) new Object[2*this.capacity];
@@ -57,6 +87,26 @@ public final class HeapQueue<P extends Comparable<P>,D> {
         public Data(P key, D data){
             this.Key = key;
             this.Data = data;
+        }
+
+        public boolean greater(HeapQueue.Data o){
+            return this.compareTo(o) >= 1;
+        }
+
+        public boolean less(HeapQueue.Data o){
+            return this.compareTo(o) <= -1;
+        }
+
+        public boolean equal(HeapQueue.Data o){
+            return this.compareTo(o) == 0;
+        }
+
+        public boolean greaterEqual(HeapQueue.Data o){
+            return this.compareTo(o) >= 0;
+        }
+
+        public boolean lessEqual(HeapQueue.Data o){
+            return this.compareTo(o) <= 0;
         }
 
         @Override
