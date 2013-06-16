21bda96769aed778aef643487fdff5eaf9c94cd9
justayak <justayak@googlemail.com>
2013-06-16 21:55:16 +0200

    jul stuff aufg9 2

---
+++ julian/a9/A2Main.java
@@ -0,0 +1,18 @@
+package a9;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Baka
+ * Date: 16.06.13
+ * Time: 21:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class A2Main {
+    public static void main(String[]a){
+
+        HeapQueue<Integer, String> h = new HeapQueue<Integer, String>();
+
+        h.store(12,"hallo");
+
+    }
+}
